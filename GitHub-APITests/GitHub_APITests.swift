89de2e011c97de30e8cc5d93cccacb3b607b2245
Abdelrhman Kamal <abdelrhmankamaleliwa@gmail.com>
2020-01-17 15:35:43 +0200

    Initial Commit

---
+++ GitHub-APITests/GitHub_APITests.swift
@@ -0,0 +1,34 @@
+//
+//  GitHub_APITests.swift
+//  GitHub-APITests
+//
+//  Created by Abdelrhman Eliwa on 1/17/20.
+//  Copyright © 2020 Abdelrhman Eliwa. All rights reserved.
+//
+
+import XCTest
+@testable import GitHub_API
+
+class GitHub_APITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
